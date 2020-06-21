01c61f728e4c4f829ce677835f0599805b25111f
devil king <surajthekeshri@gmail.com>
2020-06-21 22:38:07 +0530

    Updates

---
+++ precache-manifest.711b800c191df263066e434d6ed87eec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683f8af1943888350bb0cedd8227fa4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7cd75741c902851048e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6da25b5872ce4bb1eb49",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b7cd75741c902851048e",
+    "url": "/static/js/2.aa6df083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa6df083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da25b5872ce4bb1eb49",
+    "url": "/static/js/main.e23c53c8.chunk.js"
+  },
+  {
+    "revision": "8c542a9580f0b42d1c03",
+    "url": "/static/js/runtime-main.8fcf26b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
